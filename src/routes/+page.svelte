c58c864e1ec3d8d2f5944175140f6233254281c9
Miguel de los Reyes <thisiswhataninjalookslike@gmail.com>
2025-08-20 23:10:23 -0400

    Images in search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { filterAndRankCards } from '$lib/search';
 	import type { Card } from '$lib/types';
+	import { getHighResImage } from '$lib/utils.js';
 	import { getContext } from 'svelte';
 
 	const store = getContext<{ allCards: Card[] }>('store');
@@ -19,10 +20,23 @@
 	<input type="text" bind:value={search} />
 </div>
 
-<ul>
+<div class="card-grid">
 	{#each filteredCards as card (card.id)}
-		<li>
-			<a href={`/cards/${card.id}`}>{card.attributes.title}</a>
-		</li>
+		<a href={`/cards/${card.id}`} class="card-grid-item">
+			<img class="card" src={getHighResImage(card)} alt={card.attributes.title} loading="lazy" />
+		</a>
 	{/each}
-</ul>
+</div>
+
+<style>
+	.card-grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		gap: 1rem;
+	}
+
+	.card-grid-item {
+		min-height: 400px;
+		display: block;
+	}
+</style>
