9bd46f62fe86b889b3b53451a2ddcf10dfaab1e1
AlexR <AlexRodwell@users.noreply.github.com>
2025-08-27 00:27:52 +0100

    refactor: add size parameter to `getHighResImage`, use it in default return

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,12 +4,13 @@ export const NRDB_API_URL = 'https://api.netrunnerdb.com/api/v3/public';
 
 export const LOCAL_STORAGE_ALL_CARDS_KEY = 'allCards';
 
-export const getHighResImage = (card: Card) => {
+export const getHighResImage = (card: Card, size: 'small' | 'medium' | 'large' = 'large') => {
 	// if the card includes one of the card cycles that are released by null signal games, use the nsg image
 	const nsgCardCycles = ['elevation', 'liberation', 'borealis', 'ashes', 'system_gateway'];
+
 	if (nsgCardCycles.some((cycle) => card.attributes.card_cycle_ids.includes(cycle))) {
 		return `https://card-images.netrunnerdb.com/v2/xlarge/${card.attributes.latest_printing_id}.webp`;
 	}
 
-	return card.attributes.latest_printing_images.nrdb_classic.large;
+	return card.attributes.latest_printing_images.nrdb_classic[size];
 };
