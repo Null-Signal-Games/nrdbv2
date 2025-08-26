5bfe637838604d591fe50de70b2f16fffbb98e4f
AlexR <AlexRodwell@users.noreply.github.com>
2025-08-27 00:18:31 +0100

    feat: add cycle (set) type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -14,3 +14,43 @@ export interface Card {
 		};
 	};
 }
+
+export interface Cycle {
+	id: string;
+	type: 'card_cycles';
+	attributes: {
+		name: string;
+		date_release: string;
+		legacy_code: string;
+		card_set_ids: string[];
+		first_printing_id: string;
+		position: number;
+		released_by: string;
+		updated_at: string;
+	};
+	relationships: {
+		card_sets: {
+			links: {
+				related: string;
+			};
+		};
+		printings: {
+			links: {
+				related: string;
+			};
+		};
+		cards: {
+			links: {
+				related: string;
+			};
+		};
+		card_pools: {
+			links: {
+				related: string;
+			};
+		};
+	};
+	links: {
+		self: string;
+	};
+}
