c7c1a8fffb81bc29564f307a0418e235f7d62692
Miguel de los Reyes <thisiswhataninjalookslike@gmail.com>
2025-09-02 20:11:49 -0400

    Sort recent decklists

--- src/routes/decklists/+page.server.ts
+++ src/routes/decklists/+page.server.ts
@@ -2,7 +2,7 @@ import { NRDB_API_URL } from '$lib/utils';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	const response = await fetch(`${NRDB_API_URL}/decklists?page[size]=30`);
+	const response = await fetch(`${NRDB_API_URL}/decklists?page[size]=30&sort=-created_at`);
 	const data = await response.json();
 
 	return {
