2b0e7ba8b1b88da2be36414c8831db477ee795a9
AlexR <AlexRodwell@users.noreply.github.com>
2025-09-13 13:19:11 +0100

    refactor: update `cycles/[slug]` dynamic route to use new `<Table>` component, remove CSS

--- src/routes/cycles/[slug]/+page.svelte
+++ src/routes/cycles/[slug]/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import CardImage from '$lib/components/CardImage.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import type { Cycle, Card } from '$lib/types';
 	import { cycles, cards } from '$lib/store';
+	import Table from '$lib/components/Table.svelte';
 
 	let cycle_data = $derived<Cycle | undefined>(
 		$cycles.find((cycle: Cycle) => cycle.id === page.params.slug)
@@ -18,20 +18,5 @@
 {#if cycle_data && cards_data}
 	<Header title={`Cycle: ${cycle_data.attributes.name}`} />
 
-	<ul>
-		{#each cards_data as card (card.id)}
-			<li>
-				<CardImage {card} />
-			</li>
-		{/each}
-	</ul>
+	<Table data={cards_data} />
 {/if}
-
-<style>
-	/* Temporary styles */
-	ul {
-		display: grid;
-		grid-template-columns: repeat(4, 1fr);
-		gap: 1rem;
-	}
-</style>
