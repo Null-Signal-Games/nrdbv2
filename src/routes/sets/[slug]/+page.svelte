580a06b04f265b3ac31d576bea722e6e2f0bc461
AlexR <AlexRodwell@users.noreply.github.com>
2025-09-13 13:46:37 +0100

    refactor: update `sets/[slug]` to use new `<Table>` component, same as `cycles/[slug]`

--- src/routes/sets/[slug]/+page.svelte
+++ src/routes/sets/[slug]/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import CardImage from '$lib/components/CardImage.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import type { Set, Card } from '$lib/types';
 	import { sets, cards } from '$lib/store';
+	import Table from '$lib/components/Table.svelte';
 
 	let set_data = $derived<Set | undefined>($sets.find((set: Set) => set.id === page.params.slug));
 	let cards_data = $derived<Card[]>(
@@ -16,20 +16,5 @@
 {#if set_data && cards_data}
 	<Header title={`Set: ${set_data.attributes.name}`} />
 
-	<ul>
-		{#each cards_data as card (card.id)}
-			<li>
-				<CardImage {card} />
-			</li>
-		{/each}
-	</ul>
+	<Table data={cards_data} />
 {/if}
-
-<style>
-	/* Temporary styles */
-	ul {
-		display: grid;
-		grid-template-columns: repeat(4, 1fr);
-		gap: 1rem;
-	}
-</style>
