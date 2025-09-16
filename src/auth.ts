f11ee83250ba446588797a110581179b92a4b54c
Miguel de los Reyes <thisiswhataninjalookslike@gmail.com>
2025-09-15 23:50:39 -0400

    Read private path

--- src/auth.ts
+++ src/auth.ts
@@ -11,5 +11,17 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
 			clientId: AUTH_CLIENT_ID,
 			clientSecret: AUTH_CLIENT_SECRET
 		}
-	]
+	],
+	callbacks: {
+		async jwt({ token, account }) {
+			if (account) {
+				token.accessToken = account.access_token;
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			session.accessToken = token.accessToken as string;
+			return session;
+		}
+	}
 });
