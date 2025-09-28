d6c29f4af82ddd47a4b76cef7233243c2fe11073
AlexR <AlexRodwell@users.noreply.github.com>
2025-09-28 13:40:11 +0100

    feat: new components for decklist related elements (badges, lists, influence points)

---
+++ src/lib/components/Influence.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Card } from '$lib/types';
+
+	interface Props {
+		count: Card['attributes']['influence_cost'];
+		inline?: boolean;
+		text?: boolean;
+		total?: boolean;
+	}
+
+	let { count, text = false, total = false }: Props = $props();
+</script>
+
+<span class="influence">
+	{#if text}
+		<span class="text-sm">
+			{count}
+		</span>
+	{/if}
+	<span class="influence">
+		<span>
+			{#each Array(count) as pip (pip)}
+				●
+			{/each}
+		</span>
+		{#if total}
+			<span>
+				{#each Array(5 - count) as pip (pip)}
+					○
+				{/each}
+			</span>
+		{/if}
+	</span>
+</span>
