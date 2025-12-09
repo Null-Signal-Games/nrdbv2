a7666c8a44e7e696a6279b3cb505126e483da0b7
AlexR <AlexRodwell@users.noreply.github.com>
2025-12-09 12:28:07 +0000

    refactor: move auth provider id to env value of `AUTH_CLIENT_NAME`, renamed `AUTH_CLIENT_SECRET` to `AUTH_SECRET` to match Auth.js documentation (was also throwing an error due to this)

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,15 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
-import { AUTH_CLIENT_ID, AUTH_CLIENT_SECRET } from '$env/static/private';
+import { AUTH_CLIENT_NAME, AUTH_CLIENT_ID, AUTH_SECRET } from '$env/static/private';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	providers: [
 		{
-			id: 'nsg-keycloak',
+			id: AUTH_CLIENT_NAME,
 			name: 'Null Signal Games',
 			type: 'oidc',
 			issuer: 'https://draft-id.nullsignal.games/realms/nullsignal',
 			clientId: AUTH_CLIENT_ID,
-			clientSecret: AUTH_CLIENT_SECRET
+			clientSecret: AUTH_SECRET
 		}
 	],
 	callbacks: {
