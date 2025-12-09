165f33199e61df6176623c3e2c09bbb2052eff1e
AlexR <AlexRodwell@users.noreply.github.com>
2025-12-09 13:53:31 +0000

    feat: add `trustHost` of `true` to auth config/params, as throwing an error in staging server

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import { AUTH_CLIENT_NAME, AUTH_CLIENT_ID, AUTH_SECRET } from '$env/static/private';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
+	trustHost: true,
 	providers: [
 		{
 			id: AUTH_CLIENT_NAME,
