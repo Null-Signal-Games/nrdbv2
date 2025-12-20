802004132996ef63a63193a733a23f5967edabce
AlexR <AlexRodwell@users.noreply.github.com>
2025-12-20 15:18:26 +0000

    feat: `<Debug/>` component for storing dev actions (currently just for clearing `indexedDB` via `reset_indexeddb_data`

--- src/lib/components/Debug.svelte
+++ src/lib/components/Debug.svelte
@@ -2,4 +2,25 @@
 	import { reset_indexeddb_data } from '$lib/utils';
 </script>
 
-<button onclick={reset_indexeddb_data}>Clear IndexedDB</button>
+<div class="debug">
+	<button onclick={reset_indexeddb_data}>Clear IndexedDB</button>
+</div>
+
+<style>
+	.debug {
+		position: fixed;
+		bottom: 2rem;
+		right: 2rem;
+		display: flex;
+		flex-direction: row;
+		gap: 0.5rem;
+	}
+
+	.debug button {
+		padding: 0.5rem 0.75rem;
+		background: var(--text);
+		color: var(--foreground);
+		border: unset;
+		border-radius: 0.25rem;
+	}
+</style>
