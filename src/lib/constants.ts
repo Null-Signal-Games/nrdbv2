84cdb3dcd4bb32b8e4c7671a069b6e84ccd1a9e9
AlexR <AlexRodwell@users.noreply.github.com>
2025-12-20 15:15:45 +0000

    chore: add `APP_NAME` and `NRDB_IMAGE_URL` constant strings

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
+export const APP_NAME = 'NetrunnerDB';
+export const NRDB_IMAGE_URL = 'https://card-images.netrunnerdb.com/v2';
 export const NRDB_API_URL = 'https://api.netrunnerdb.com/api/v3/public';
 export const NRDB_PRIVATE_API_URL = 'https://api-preview.netrunnerdb.com/api/v3/private';
 export const NRDB_CLASSIC_URL = 'https://netrunnerdb.com/en';
