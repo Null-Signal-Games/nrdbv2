8cbbec302a9d95bba6392c0221c0c5bf523b0c35
AlexR <AlexRodwell@users.noreply.github.com>
2025-12-20 15:27:47 +0000

    feat(auth): add basic keycloak state debug (`user`, `expiration`, and `accessToken`)

--- src/lib/components/Debug.svelte
+++ src/lib/components/Debug.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { reset_indexeddb_data } from '$lib/utils';
+	import { page } from '$app/state';
 </script>
 
 <div class="debug">
 	<button onclick={reset_indexeddb_data}>Clear IndexedDB</button>
+	<details>
+		<summary>Session Data</summary>
+		<pre>{JSON.stringify(page.data.session, null, 2)}</pre>
+	</details>
 </div>
 
 <style>
@@ -16,7 +21,7 @@
 		gap: 0.5rem;
 	}
 
-	.debug button {
+	.debug :where(button, details) {
 		padding: 0.5rem 0.75rem;
 		background: var(--text);
 		color: var(--foreground);
