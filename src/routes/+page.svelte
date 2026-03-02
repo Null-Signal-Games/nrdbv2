3606a2a51b82f3579de9f05e88a398b502e5d810
AlexR <AlexRodwell@users.noreply.github.com>
2026-03-02 13:08:57 +0000

    feat: add `Container` component wrapper to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
     import Factions from "$lib/components/Factions.svelte";
     import { localizeHref } from "$lib/paraglide/runtime";
     import DecklistPreview from "$lib/components/decklist/Preview.svelte";
+    import Container from "$lib/components/Container.svelte";
 
     interface Props {
         data: {
@@ -20,43 +21,45 @@
     let { data }: Props = $props();
 </script>
 
-<Factions />
+<Container>
+    <Factions />
 
-<div class="home container">
-    <div>
-        {#await data.decklist_of_the_week}
-            <p>Loading deck of the week...</p>
-        {:then deck_of_the_week_data}
-            <h2>Deck of the Week</h2>
-            <DecklistSummary
-                identity={deck_of_the_week_data.identity}
-                decklist={deck_of_the_week_data.decklist}
-            />
-            <DecklistBreakdown
-                decklist={deck_of_the_week_data.decklist}
-                cards={deck_of_the_week_data.cards}
-            />
-        {:catch error}
-            <p>error loading deck of the day: {error.message}</p>
-        {/await}
-    </div>
-    <div>
-        Latest decks
-        <ul>
-            {#await data.decklists}
-                Loading decklists...
-            {:then decklists}
-                {#each decklists as decklist (decklist.id)}
-                    <li>
-                        <DecklistPreview {decklist} />
-                    </li>
-                {/each}
+    <div class="home">
+        <div>
+            {#await data.decklist_of_the_week}
+                <p>Loading deck of the week...</p>
+            {:then deck_of_the_week_data}
+                <h2>Deck of the Week</h2>
+                <DecklistSummary
+                    identity={deck_of_the_week_data.identity}
+                    decklist={deck_of_the_week_data.decklist}
+                />
+                <DecklistBreakdown
+                    decklist={deck_of_the_week_data.decklist}
+                    cards={deck_of_the_week_data.cards}
+                />
             {:catch error}
-                <p>error loading decklists: {error.message}</p>
+                <p>error loading deck of the day: {error.message}</p>
             {/await}
-        </ul>
+        </div>
+        <div>
+            Latest decks
+            <ul>
+                {#await data.decklists}
+                    Loading decklists...
+                {:then decklists}
+                    {#each decklists as decklist (decklist.id)}
+                        <li>
+                            <DecklistPreview {decklist} />
+                        </li>
+                    {/each}
+                {:catch error}
+                    <p>error loading decklists: {error.message}</p>
+                {/await}
+            </ul>
+        </div>
     </div>
-</div>
+</Container>
 
 <style>
     /* Temporary styles */
