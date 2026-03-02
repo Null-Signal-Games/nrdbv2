4db671caf7fd6ccbd6e0ee9de62ce607d176d275
AlexR <AlexRodwell@users.noreply.github.com>
2026-03-02 13:08:30 +0000

    feat: add locale extraction to layout server load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,12 @@
 import type { LayoutServerLoad } from './$types';
+import { extractLocaleFromRequest, serverAsyncLocalStorage } from '$lib/paraglide/runtime';
 
 export const load: LayoutServerLoad = async (event) => {
+	const locale =
+		serverAsyncLocalStorage?.getStore()?.locale ?? extractLocaleFromRequest(event.request);
+
 	return {
+		locale,
 		session: await event.locals.auth()
 	};
 };
