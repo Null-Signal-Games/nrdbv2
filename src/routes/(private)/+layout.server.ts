fe5af25e90c94ca4779c2e239131641b4a96a31e
AlexR <AlexRodwell@users.noreply.github.com>
2026-04-06 16:53:03 +0100

    feat: basic `(private)/` dir setup for auth user data

---
+++ src/routes/(private)/+layout.server.ts
@@ -0,0 +1,37 @@
+import type { LayoutServerLoad } from './$types';
+import { NRDB_PRIVATE_API_URL } from '$lib/constants';
+import type { ApiResponse, Decklist } from '$lib/types';
+
+export const load: LayoutServerLoad = async (event) => {
+	const session = await event.locals.auth();
+	console.log('session', session);
+
+	if (!session?.accessToken) {
+		return {
+			decks: []
+		};
+	}
+
+	const res = await event.fetch(`${NRDB_PRIVATE_API_URL}/decklists`, {
+		headers: {
+			Authorization: `Bearer ${session.accessToken}`
+		}
+	});
+
+	if (res.status === 401) {
+		return {
+			decks: []
+		};
+	}
+
+	if (!res.ok) {
+		console.error(`HTTP error! status: ${res.status}`);
+		return {
+			decks: []
+		};
+	}
+
+	const decks = (await res.json()) as ApiResponse<Decklist[]>;
+
+	return { decks: decks.data };
+};
---
+++ src/routes/(private)/decks/[id]/+page.svelte
@@ -0,0 +1,1 @@
+View deck [id]
